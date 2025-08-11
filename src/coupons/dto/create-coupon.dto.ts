1a1169f47a1c79e4a116edf03947d9ed982c0325
Miguel Angel Ureña Batista <miguel02urena@gmail.com>
2025-08-11 13:42:54 -0600

    Coupon dto

--- src/coupons/dto/create-coupon.dto.ts
+++ src/coupons/dto/create-coupon.dto.ts
@@ -1,1 +1,16 @@
-export class CreateCouponDto {}
+import { IsDateString, IsInt, IsNotEmpty, Max, Min } from 'class-validator';
+
+export class CreateCouponDto {
+  @IsNotEmpty({ message: 'El nombre del cupón es obligatorio' })
+  name: string;
+
+  @IsNotEmpty({ message: 'El descuento no puede ir vacio' })
+  @IsInt({ message: 'El descuento debe ser entre 1 y 100 en enteros' })
+  @Max(100, { message: ' El descuento máximo es de 100' })
+  @Min(1, { message: 'El descuento minimo es de 1' })
+  percentage: number;
+
+  @IsNotEmpty({ message: 'La fecha de expiracion no debe ser vacia' })
+  @IsDateString({}, { message: 'Fecha no válida' })
+  expirationDate: Date;
+}
