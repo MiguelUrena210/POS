95d824c91a4f9de7158a0ff1c74d6a8c6e74b6e2
Miguel Angel Ureña Batista <miguel02urena@gmail.com>
2025-08-14 09:44:37 -0600

    Update Seeder - Add injection data

--- src/seeder/seeder.service.ts
+++ src/seeder/seeder.service.ts
@@ -1,8 +1,38 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Category } from '../categories/entities/category.entity';
+import { Product } from '../products/entities/product.entity';
+import { Repository } from 'typeorm';
+import { categories } from './data/categories';
+import { products } from './data/products';
 
 @Injectable()
 export class SeederService {
-    async seed() {
-        console.log('Desde seed en SeederService')
+  constructor(
+    @InjectRepository(Category)
+    private readonly categoryRepository: Repository<Category>,
+    @InjectRepository(Product)
+    private readonly productRepository: Repository<Product>,
+  ) {}
+  async seed() {
+    console.log('Desde seed en SeederService');
+    await this.categoryRepository.save(categories);
+
+    for await (const seedProduct of products) {
+      const category = await this.categoryRepository.findOneBy({
+        id: seedProduct.categoryId,
+      });
+
+      if (category) {
+        const product = new Product();
+        product.name = seedProduct.name;
+        product.image = seedProduct.image;
+        product.price = seedProduct.price;
+        product.inventory = seedProduct.inventory;
+        product.category = category;
+
+        await this.productRepository.save(product)
+      }
     }
+  }
 }
