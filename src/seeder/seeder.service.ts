7f1d7a53e9fbee494f5afdb26ee696cf3ed2e56e
Miguel Angel Ureña Batista <miguel02urena@gmail.com>
2025-08-19 11:01:11 -0600

    Add db reset into onModuleIniti

--- src/seeder/seeder.service.ts
+++ src/seeder/seeder.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Category } from '../categories/entities/category.entity';
 import { Product } from '../products/entities/product.entity';
-import { Repository } from 'typeorm';
+import { Repository, DataSource } from 'typeorm';
 import { categories } from './data/categories';
 import { products } from './data/products';
 
@@ -13,7 +13,14 @@ export class SeederService {
     private readonly categoryRepository: Repository<Category>,
     @InjectRepository(Product)
     private readonly productRepository: Repository<Product>,
+    private dataSource: DataSource,
   ) {}
+
+  async onModuleInit() {
+    const conection = this.dataSource;
+    await conection.dropDatabase();  // elimina la base de datos
+    await conection.synchronize(); // considerando las entidades y las relaciones definidas, crea nuevamente la base de datos
+  }
   async seed() {
     console.log('Desde seed en SeederService');
     await this.categoryRepository.save(categories);
